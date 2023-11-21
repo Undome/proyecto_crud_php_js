04580f5ef72e652498ba3df3a6d8f1b9c024cf4c
Ark <arkdan@gmail.com>
2023-11-21 17:05:26 -0600

    cambio

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Curd with javascript fetch</title>
+    <title>Crud con Php y Javascript</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 
