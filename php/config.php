cc84bf7641fe7bfda0b264c64e5f1793924ca354
Ark <arkdan@gmail.com>
2023-11-21 16:26:05 -0600

    Cambio de configuración de base de datos

--- php/config.php
+++ php/config.php
@@ -1,9 +1,9 @@
 <?php
 
 $localhost="localhost";
-$username="root";
-$password="";
-$database="php_fetch_crud";
+$username="id20493902_root";
+$password="L3chug@3";
+$database="id20493902_php_fetch_crud";
 
 $conn=mysqli_connect($localhost,$username,$password,$database);
 
